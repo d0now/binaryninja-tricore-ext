1860e63788073f0e6b29d098273b523ba6c517f9
Dohyun Kim <dolpin1402@gmail.com>
2025-01-22 00:01:50 +0900

    add absolute call fix

--- __init__.py
+++ __init__.py
@@ -2,10 +2,16 @@ from binaryninja.architecture import Architecture
 from binaryninja.plugin import PluginCommand
 
 from .tcext.absolute_addressing import AbsoluteAddressingHook
+from .tcext.absolute_call import AbsoluteCallHook
 
-# workaround for TriCore architecture not being recognized by Binary Ninja at initial time (maybe not a core architecture?)
+def enable():
+    AbsoluteAddressingHook(Architecture['tricore']).register()
+    AbsoluteCallHook(Architecture['tricore']).register()
+
+# workaround for TriCore architecture not being recognized by Binary Ninja at plugin loading time
+# (maybe not a core architecture?)
 PluginCommand.register(
     "TriCore Extension Architecture Hook",
     "",
-    lambda _: AbsoluteAddressingHook(Architecture['tricore']).register()
+    lambda _: enable()
 )
